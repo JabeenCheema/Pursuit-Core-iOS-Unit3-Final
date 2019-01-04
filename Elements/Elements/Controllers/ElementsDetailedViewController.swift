a1d9f6b30811ef7ca0eab2355f0bd7067764d1d1
Jabeen Cheema <jabeencheema@pursuit.org>
2019-01-04 17:55:39 -0500

    Second Commit

--- Elements/Elements/Controllers/ElementsDetailedViewController.swift
+++ Elements/Elements/Controllers/ElementsDetailedViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class ElementsDetailedViewController: UIViewController {
 
+    
+    @IBOutlet weak var elementSymbol: UILabel!
+    @IBOutlet weak var elementNumber: UILabel!
+    @IBOutlet weak var elementWeight: UILabel!
+    @IBOutlet weak var elementMeltingPoint: UILabel!
+    @IBOutlet weak var elementBoilingPoint: UILabel!
+    @IBOutlet weak var elementDiscoveryby: UILabel!
+    
     public var element: Element!
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = element.name
        
     }
     
+    private func updateUI(){
+        guard let element = element else {
+            fatalError("element is nil")
+        }
+    
+    }
 
     
 

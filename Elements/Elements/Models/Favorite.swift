a7835b0e5bd94e5f37431aee19024e3ad4c465ef
Jabeen Cheema <jabeencheema@pursuit.org>
2019-01-07 12:51:51 -0500

    Tab Bar Controller added and Favorite VC

---
+++ Elements/Elements/Models/Favorite.swift
@@ -0,0 +1,13 @@
+//
+//  Favorite.swift
+//  Elements
+//
+//  Created by Jabeen's MacBook on 1/7/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import Foundation
+
+struct Favorite: Codable {
+    
+}
